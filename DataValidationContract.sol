591332907ab65e1179c5a30abc2247f465799bdd
ArthurBonsu <peeman34@gmail.com>
2023-12-09 06:36:54 +0800

    Add files via upload

---
+++ DataValidationContract.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./OracleConfigurationContract.sol";
+
+contract DataValidationContract {
+
+    // Reference to the OracleConfigurationContract
+    OracleConfigurationContract public oracleConfigurationContract;
+
+    // Event emitted when data validation is successful
+    event DataValidationSuccessful(bytes32 requestId);
+
+    // Event emitted when data validation fails
+    event DataValidationFailed(bytes32 requestId);
+
+    // Constructor to set the OracleConfigurationContract
+    constructor(address oracleConfigAddress) {
+        oracleConfigurationContract = OracleConfigurationContract(oracleConfigAddress);
+    }
+
+
+   // Function to check if an address has a specific role
+    function hasRole(address oracleAddress, OracleConfigurationContract.OracleRole role) public  returns (bool) {
+        return oracleConfigurationContract.hasRole(oracleAddress, role);
+    }
+
+    // Function to validate the collected data
+    function validateData(bytes memory data, bytes32 requestId) public {
+        require(hasRole(msg.sender, OracleConfigurationContract.OracleRole.VALIDATOR), "Address not authorized to validate data");
+
+        // Implement custom validation logic based on the specific data being collected
+        // For example, validate data fields, check against reference data sources, etc.
+        bool isValid = performValidationLogic(data);
+
+        // Emit events based on validation result
+        if (isValid) {
+            emit DataValidationSuccessful(requestId);
+        } else {
+            emit DataValidationFailed(requestId);
+        }
+    }
+
+    // Internal function to perform custom validation logic
+    function performValidationLogic(bytes memory data) internal view returns (bool) {
+        // Replace with actual validation logic
+        return true;
+    }
+
+ 
+}
